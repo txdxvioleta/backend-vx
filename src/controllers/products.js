b081592be68daeb5e6bb73552abfc4b9db71094d
Gonzalo Caram <gcaram95@gmail.com>
2022-10-07 03:42:02 -0300

    correción de funciones, y agregada la funcionalidad de recuperar producto por categoría.

--- src/controllers/products.js
+++ src/controllers/products.js
@@ -17,25 +17,18 @@ const getAllProducts = async (req, res) => {
   });
 };
 
-const getProductByCategory = async (req, res) => {  
-  // pagination:
-  let limit = '';
-
-  if (req.query.page) {
-    const size = 5;
-    const operation = (req.query.page - 1) * size;
-    limit += `LIMIT ${operation}, ${size}`;
-  }
+const getProductByCategory = async (req, res) => {
   await connection.query(
-    `SELECT * FROM productos WHERE categoria = '${req.params.category}' ${limit}`,
+    `SELECT * FROM productos p,categorias c 
+  WHERE c.id_categoria = p.id_categ AND c.nombre_categ = '${req.params.category}'`,
     (error, result) => {
-      !error && result.length > 0 ? res.status(200).json(result) : res.status(404).json('No results');
+      !error && result.length > 0 ? res.status(200).json(result) : res.status(404).json('Category not found');
     }
   );
 };
 
 const getProductById = async (req, res) => {
-  await connection.query(`SELECT * FROM productos WHERE id_producto = ${req.params.id}`, (error, result) => {
+  await connection.query(`SELECT * FROM productos WHERE id_prod = ${req.params.id}`, (error, result) => {
     !error && result.length > 0 ? res.status(200).json(result) : res.status(404).json('No results');
   });
 };
@@ -48,7 +41,7 @@ const addProduct = async (req, res) => {
 
 const updateProduct = async (req, res) => {
   await connection.query(
-    `UPDATE productos SET ? WHERE id_producto = ?`,
+    `UPDATE productos SET ? WHERE id_prod = ?`,
     [req.body, req.params.id],
     (error, result) => {
       !error && result.affectedRows > 0
@@ -59,7 +52,7 @@ const updateProduct = async (req, res) => {
 };
 
 const deleteProduct = async (req, res) => {
-  await connection.query(`DELETE FROM productos WHERE id_producto = ?`, [req.params.id], (error, result) => {
+  await connection.query(`DELETE FROM productos WHERE id_prod = ?`, [req.params.id], (error, result) => {
     !error && result.affectedRows > 0
       ? res.status(202).json('Deleted successfully')
       : res.status(404).json('No results');
